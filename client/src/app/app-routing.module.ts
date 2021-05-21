9bfd6dc76bec89e8a22813698efd021d0fe44d11
Haro <piricharun@gmail.com>
2021-05-21 03:29:42 +0200

    crimefeed button

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthenticationGuard } from './services/authentication.guard';
 import { GameContainerComponent } from './components/game-container/game-container.component';
 import { GamesListComponent } from './components/games-list/games-list.component';
+import { CrimeFeedComponent } from './components/crime-feed/crime-feed.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [ AuthenticationGuard ] },
   { path: 'games', component: GamesListComponent, canActivate: [ AuthenticationGuard ] },
   { path: 'games/:id', component: GameContainerComponent, canActivate: [ AuthenticationGuard ] },
+  { path: 'crime-feed', component: CrimeFeedComponent, canActivate: [ AuthenticationGuard ] },
   { path: 'login', component: LoginComponent },
 ];
 
